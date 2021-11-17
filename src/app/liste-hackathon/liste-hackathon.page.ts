0d262d3ec7bc54e47e070785ad7fb243bcf77851
Pierre MURAIL <PMURAIL>
2021-11-17 14:56:19 +0100

    page listHackathon créer

---
+++ src/app/liste-hackathon/liste-hackathon.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-hackathon',
+  templateUrl: './liste-hackathon.page.html',
+  styleUrls: ['./liste-hackathon.page.scss'],
+})
+export class ListeHackathonPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
